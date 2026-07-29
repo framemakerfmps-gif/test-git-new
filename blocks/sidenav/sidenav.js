4007c24491f5fe4e5ca9580d5167d442f75105d2
framemakerfmps-gif <framemakerfmps@gmail.com>
2026-07-29 14:07:35 +0530

    uploading sidenav to: https://api.github.com/repos/framemakerfmps-gif/test-git-new/contents/blocks/sidenav/sidenav.js

--- blocks/sidenav/sidenav.js
+++ blocks/sidenav/sidenav.js
@@ -1,6 +1,6 @@
 import { migrateTree } from "../utils.js";
 const treeData = [{"displayName":"Adobe India","url":"contents/adobe-india"}]
-const mapTitle = "html5_generation"
+const mapTitle = "html5_generation_sachin"
 const isDesktop = window.matchMedia("(min-width: 900px)");
 
 function expandHeirarchy(element, root) {
